5f81622a4abcd0396bc2638312abfb299de7663b
wanghe_global <1570128795@qq.com>
2019-03-24 00:01:28 +0800

    2019.3.23 后台页面引入

---
+++ runtime/temp/4b845779662f429a363282b9fb39f84c.php
@@ -0,0 +1,301 @@
+<?php /*a:3:{s:60:"D:\wamp64\www\MyBlog\application\admin\view\index\index.html";i:1553355744;s:59:"D:\wamp64\www\MyBlog\application\admin\view\common\top.html";i:1553355264;s:60:"D:\wamp64\www\MyBlog\application\admin\view\common\left.html";i:1553356776;}*/ ?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<title>后台管理系统 - Mr.Wang - Blog</title>
+
+		<meta name="description" content="Dashboard">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<!--引入public目录中的ico图标-->
+		<link rel="shortcut icon" href="http://localhost/myblog/public/static/admin/images/favicon.ico">
+		<!--Basic http://localhost/myblog/public/static/admin/styles-->
+		<link href="http://localhost/myblog/public/static/admin/style/bootstrap.css" rel="stylesheet">
+		<link href="http://localhost/myblog/public/static/admin/style/font-awesome.css" rel="stylesheet">
+		<link href="http://localhost/myblog/public/static/admin/style/weather-icons.css" rel="stylesheet">
+
+		<!--Beyond http://localhost/myblog/public/static/admin/styles-->
+		<link id="beyond-link" href="http://localhost/myblog/public/static/admin/style/beyond.css" rel="stylesheet" type="text/css">
+		<link href="http://localhost/myblog/public/static/admin/style/demo.css" rel="stylesheet">
+		<link href="http://localhost/myblog/public/static/admin/style/typicons.css" rel="stylesheet">
+		<link href="http://localhost/myblog/public/static/admin/style/animate.css" rel="stylesheet">
+
+	</head>
+
+	<body>
+		<!-- 头部 -->
+	<div class="navbar">
+    <div class="navbar-inner">
+        <div class="navbar-container">
+            <!-- Navbar Barnd -->
+            <div class="navbar-header pull-left">
+                <a href="#" class="navbar-brand">
+                    <small>
+                            <img src="http://localhost/myblog/public/static/admin/images/logo.png" alt="">
+                        </small>
+                </a>
+            </div>
+            <!-- /Navbar Barnd -->
+            <!-- Sidebar Collapse -->
+            <div class="sidebar-collapse" id="sidebar-collapse">
+                <i class="collapse-icon fa fa-bars"></i>
+            </div>
+            <!-- /Sidebar Collapse -->
+            <!-- Account Area and Settings -->
+            <div class="navbar-header pull-right">
+                <div class="navbar-account">
+                    <ul class="account-area">
+                        <li>
+                            <a class="login-area dropdown-toggle" data-toggle="dropdown">
+                                <div class="avatar" title="View your public profile">
+                                    <img src="http://localhost/myblog/public/static/admin/images/newx.jpg">
+                                </div>
+                                <section>
+                                    <h2><span class="profile"><span><?php echo htmlentities(app('request')->session('username')); ?></span></span></h2>
+                                </section>
+                            </a>
+                            <!--Login Area Dropdown-->
+                            <ul class="pull-right dropdown-menu dropdown-arrow dropdown-login-area">
+                                <li class="username"><a>David Stevenson</a></li>
+                                <li class="dropdown-footer">
+                                    <a href="<?php echo url('admin/logout'); ?>">
+                                            退出登录
+                                        </a>
+                                </li>
+                                <li class="dropdown-footer">
+                                    <a href="<?php echo url('admin/edit',array('id'=>app('request')->session('uid'))); ?>">
+                                            修改密码
+                                        </a>
+                                </li>
+                            </ul>
+                            <!--/Login Area Dropdown-->
+                        </li>
+                        <!-- /Account Area -->
+                        <!--Note: notice that setting div must start right after account area list.
+                            no space must be between these elements-->
+                        <!-- Settings -->
+                    </ul>
+                </div>
+            </div>
+            <!-- /Account Area and Settings -->
+        </div>
+    </div>
+</div>
+
+		<!-- /头部 -->
+
+		<div class="main-container container-fluid">
+			<div class="page-container">
+				<!-- Page Sidebar -->
+				<div class="page-sidebar" id="sidebar">
+					<!-- Page Sidebar Header-->
+					<div class="sidebar-header-wrapper">
+						<input class="searchinput" type="text" disabled="disabled">
+					</div>
+					<!-- /Page Sidebar Header -->
+					 <!-- Sidebar Menu -->
+                <ul class="nav sidebar-menu">
+                    <!--Dashboard-->
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-user"></i>
+                            <span class="menu-text">管理员</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('admin/lst'); ?>">
+                                    <span class="menu-text">
+                                        资料修改                                   </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li> 
+					 <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-list"></i>
+                            <span class="menu-text">栏目管理</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('cate/lst'); ?>">
+                                    <span class="menu-text">
+                                        栏目列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li> 
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-file-text"></i>
+                            <span class="menu-text">文章管理</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('article/lst'); ?>">
+                                    <span class="menu-text">
+                                        文章列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li> 
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-comment"></i>
+                            <span class="menu-text">评论管理</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('article/lst'); ?>">
+                                    <span class="menu-text">
+                                        文章评论                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo url('article/lst'); ?>">
+                                    <span class="menu-text">
+                                        留言板                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li> 
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-flag"></i>
+                            <span class="menu-text">项目管理</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('article/lst'); ?>">
+                                    <span class="menu-text">
+                                        项目列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo url('article/lst'); ?>">
+                                    <span class="menu-text">
+                                        项目统计                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li> 
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-book"></i>
+                            <span class="menu-text">日志管理</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('article/lst'); ?>">
+                                    <span class="menu-text">
+                                        日志列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li> 
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-calendar"></i>
+                            <span class="menu-text">随笔管理</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('article/lst'); ?>">
+                                    <span class="menu-text">
+                                        随笔列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li> 
+                    
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-gear"></i>
+                            <span class="menu-text">系统</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('tags/lst'); ?>">
+                                    <span class="menu-text">
+                                        网站资料                                  </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo url('tags/lst'); ?>">
+                                    <span class="menu-text">
+                                        关于                                  </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo url('tags/lst'); ?>">
+                                    <span class="menu-text">
+                                       友情链接                                  </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li>                        
+                    
+                                           
+                    
+                </ul>
+                <!-- /Sidebar Menu -->
+            </div>
+					<!-- /Page Sidebar -->
+					<!-- Page Content -->
+					<div class="page-content">
+						<!-- Page Breadcrumb -->
+						<div class="page-breadcrumbs">
+							<ul class="breadcrumb">
+								<li class="active">控制面板</li>
+							</ul>
+						</div>
+						<!-- /Page Breadcrumb -->
+
+						<!-- Page Body -->
+						<div class="page-body">
+
+							<div style="text-align:center; line-height:1000%; font-size:24px;">
+								Mr.Wang - Blog<br>
+								<p style="color:#aaa;">后台管理系统</p>
+							</div>
+						</div>
+
+					</div>
+					<!-- /Page Body -->
+				</div>
+				<!-- /Page Content -->
+			</div>
+		</div>
+
+		<!--Basic Scripts-->
+		<script src="http://localhost/myblog/public/static/admin/style/jquery_002.js"></script>
+		<script src="http://localhost/myblog/public/static/admin/style/bootstrap.js"></script>
+		<script src="http://localhost/myblog/public/static/admin/style/jquery.js"></script>
+		<!--Beyond Scripts-->
+		<script src="http://localhost/myblog/public/static/admin/style/beyond.js"></script>
+
+	</body>
+
+</html>
